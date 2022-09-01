746d0dc14e8e451a967e319f4027666cf5da25ae
Deividgp <dayd4@outlook.com>
2022-09-01 11:19:58 +0200

    socket baseURL changed

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("/", { autoConnect: false });
+const baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:4000";
+
+const socket = io(baseURL, { autoConnect: false });
 
 export default socket;
