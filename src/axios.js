7701c3f17355b7617aa0c35e385c169a506c25ee
Deividgp <dayd4@outlook.com>
2022-09-01 11:16:47 +0200

    with credentials removed

--- src/axios.js
+++ src/axios.js
@@ -3,6 +3,5 @@ import axios from 'axios';
 const baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:4000";
 
 export default axios.create({
-    baseURL: baseURL,
-    withCredentials: true
+    baseURL: baseURL
 });
