d110fdc8b80010cf7eab595263a9096996ade3cd
Deividgp <dayd4@outlook.com>
2022-09-01 10:06:17 +0200

    base_url env variable updated

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.BASE_URL || "http://localhost:4000";
+const baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:4000";
 
 export default axios.create({
     baseURL: baseURL,
